5dff3b3a6953ea496265234c8e43a267ae5ab43d
seplangluos <seplan.gluos@valadares.mg.gov.br>
2026-07-20 11:33:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -87,6 +87,8 @@ const GLUOS_DATA = {
     {id: 7, texto: "Passo (outros setores)"},
     {id: 8, texto: "Tranferido"},
     {id: 9, texto: "Visita de vistoria"},
+    {id: 9, texto: "Conformidade/Antena"},
+    {id: 9, texto: "Declarações"},
   ]
 };
 
