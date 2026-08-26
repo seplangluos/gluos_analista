3387a99be5156eaf5909e4c1a4b90560e6cfb9c2
seplangluos <seplan.gluos@valadares.mg.gov.br>
2026-08-26 17:09:47 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ const GLUOS_DATA = {
     {id: 1, texto: "Deferimento de Processo"},
     {id: 2, texto: "Notificação de processo"},
     {id: 3, texto: "Enc. a GCT/Topografia (passo)"},
-    {id: 4, texto: "Passo para Vistoria"},
+    {id: 4, texto: "Enc. Vistoria"},
     {id: 5, texto: "Atendimento (RT/Contribuinte)"},
     {id: 6, texto: "Indeferimento de Processo"},
     {id: 7, texto: "Passo (outros setores)"},
@@ -95,6 +95,7 @@ const GLUOS_DATA = {
     {id: 13, texto: "Situação do imóvel"},
     {id: 14, texto: "Atendimento"},
     {id: 15, texto: "Atendimento sem processo"},
+    {id: 16, texto: "Passo para Vistoria"},
   ]
 };
 
